100b0c1d81e42e579876f615ca3ccb16707c3652
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-05-23 21:59:45 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -53,7 +53,7 @@ def pregunta_02():
     print(df.shape
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(round(np.corrcoef(df['life'],df['fertility'])[1,0],4))
+    print(np.corrcoef(df['life'],df['fertility'])[1,0].round(4))
 
     # Imprima la media de la columna `life` con 4 decimales.
     print(round(df['life'].mean(),4))
