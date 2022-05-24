b69ea2c7124c2d27cb28099809480e52f0975369
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-05-23 22:06:13 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -15,8 +15,8 @@ def pregunta_01():
     Complete el código presentado a continuación.
     """
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("gm_2008_region.csv")
-
+    df = pd.read_csv('gm_2008_region.csv', sep=',',thousands=None,decimal='.')
+    
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
     y = df['life'].values
     X = df['fertility'].values
@@ -28,10 +28,10 @@ def pregunta_01():
     print(X.shape)
 
     # Transforme `y` a un array de numpy usando reshape
-    y_reshaped = np.reshape(y, (-1,1))
+    y_reshaped = y.reshape(-1,1)
 
     # Trasforme `X` a un array de numpy usando reshape
-    X_reshaped = np.reshape(X, (-1,1))
+    X_reshaped = X.reshape(-1,1)
 
     # Imprima las nuevas dimensiones de `y`
     print(y_reshaped.shape)
@@ -47,7 +47,7 @@ def pregunta_02():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region.csv')
+    df = pd.read_csv('gm_2008_region.csv', sep=',',thousands=None,decimal='.')
 
     # Imprima las dimensiones del DataFrame
     print(df.shape)
@@ -72,7 +72,7 @@ def pregunta_03():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region.csv')
+    df = pd.read_csv('gm_2008_region.csv', sep=',',thousands=None,decimal='.')
 
     # Asigne a la variable los valores de la columna `fertility`
     X_fertility = df['fertility'].values
@@ -117,7 +117,7 @@ def pregunta_04():
     from sklearn.model_selection import train_test_split
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region.csv')
+    df = pd.read_csv('gm_2008_region.csv', sep=',',thousands=None,decimal='.')
 
     # Asigne a la variable los valores de la columna `fertility`
     X_fertility = df['fertility'].values
